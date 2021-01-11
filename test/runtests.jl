b8697a6f9ef96710f0eab9680e5de196130caf78
andreas <andreas.koher@gmail.com>
2021-01-11 18:27:32 +0100

    add savetestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,3 @@
-using Rethinking
-using Test
-using Turing
-using DataFrames
-using PrettyTables
+using SafeTestsets
 
-@testset "Rethinking.jl" begin
-    @model function test(y)
-        μ ~ Normal(10, 1)
-        σ ~ Exponential(5)
-        y .~ Normal(μ, σ)
-    end
-    y = randn(1000)*5 .+ 10
-    res = quap(test(y))
-    μ, σ = res.coef.μ, res.coef.σ
-    @test round(μ) == 10
-    @test round(σ) == 5
-
-    df = DataFrame((a=rand(1000)))
-    @test isnothing(precise(df))
-    """
-    ┌───────┬───────────────────────────────────────────┐
-    │ param │ mean   std  5.5%   50%  94.5%   histogram │
-    ├───────┼───────────────────────────────────────────┤
-    │     a │ 0.51  0.28  0.06  0.52   0.95  ▆▇▇▇▇▇█▇▆▇ │
-    └───────┴───────────────────────────────────────────┘
-    """
-end
+@safetestset "test1" begin include("test1.jl") end
